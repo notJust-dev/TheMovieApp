1ff43947ba18a9c293632426c2ceddaa1adbcfd9
Vadim Savin <savinvadim1312@gmail.com>
2024-03-01 16:43:53 +0000

    fetch movie details

--- api/movies.ts
+++ api/movies.ts
@@ -1,15 +1,17 @@
 const apiKey =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjhhMTE5YzAzODNlNWUxM2VkODQxNjY4ZWRlNzU4MiIsInN1YiI6IjY1ZTFlN2FhNDFhNTYxMDE4Njg0MGVlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UJVtqRNQ-1QFfb4733gzUQ5VgFYFj1Iimk_1GvDwSMo';
 
+const headers = {
+  accept: 'application/json',
+  Authorization: 'Bearer ' + apiKey,
+};
+
 export const fetchTopRatedMovies = async () => {
   const url =
     'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
     method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer ' + apiKey,
-    },
+    headers,
   };
   const res = await fetch(url, options);
 
@@ -19,4 +21,21 @@ export const fetchTopRatedMovies = async () => {
 
   const json = await res.json();
   return json.results;
+};
+
+export const fetchMovie = async (id: number) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers,
+  };
+
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch movies');
+  }
+
+  const json = await res.json();
+  return json;
 };
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,27 @@
+import { fetchMovie } from '@/api/movies';
+import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['movies', id],
+    queryFn: () => fetchMovie(id),
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch data</Text>;
+  }
+
   return (
     <View>
-      <Text>MovieDetails: {id}</Text>
+      <Text style={{ fontSize: 24, fontWeight: '500' }}>{data.title}</Text>
     </View>
   );
 };
